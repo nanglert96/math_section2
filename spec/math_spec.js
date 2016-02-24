fed38f84e11849eb67896ad481b0d4eb9b3da45d
Ryan Nangle <ryannangle@Ryans-MacBook-Pro.local>
2016-02-24 11:03:47 -0500

    added tests -, *, /, and code to pass

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,5 +1,30 @@
+// tell the spec where to find our code
+
+var math = require('../math');
+
 describe("A simple arithmetic program", function() {
   it("can add two numbers", function() {
-    expect(add(3, 5)).toBe(8);
+    expect(math.add(3, 5)).toBe(8);
+  });
+});
+
+
+describe("A simple arithmetic program", function() {
+  it("can subtract two numbers", function() {
+    expect(math.subtract(5, 3)).toBe(2);
+  });
+});
+
+
+describe("A simple arithmetic program", function() {
+  it("can multiply two numbers", function() {
+    expect(math.multiply(5, 3)).toBe(15);
+  });
+});
+
+
+describe("A simple arithmetic program", function() {
+  it("can divide two numbers", function() {
+    expect(math.divide(10, 2)).toBe(5);
   });
 });
